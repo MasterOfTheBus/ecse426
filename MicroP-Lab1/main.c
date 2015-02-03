e2bb67b6a6b2d53430a40b54ae05a9f3cef35193
unknown <sng20@156TR4160MP-08.campus.MCGILL.CA>
2015-02-03 16:03:19 -0500

    c version of the filter working

--- MicroP-Lab1/main.c
+++ MicroP-Lab1/main.c
@@ -27,57 +27,30 @@ int Kalmanfilter_C (float* InputArray, float* OutputArray, kalman_state* kstate,
 	return 0;
 }
 
-typedef struct example example;
-struct example {
-	int a;
-	int b;
-	int c;
-};
-
-int exampleFilter(int* input, int* output, example* exState, int length) {
-	int i = 0;
-	while (i < length) {
-		exState->a = exState->a + exState->b;
-		exState->b = exState->b / exState->c + exState->a;
-		exState->c = exState->c + exState->b + input[i];
-		output[i] = exState->c;
-	}
-	return 0;
-}
-
 int main()
 {
-	int input[3];
-	int output[3];
-	/*
+	
 	float input[3];
-	float output[3];*/
-	/*kalman_state* kstate;
-	kstate->q = 0.1;
-	kstate->r = 0.1;
-	kstate->p = 0.1;
-	kstate->k = 0;
-	kstate->x = 0;
-	
-	float input[] = {1.0, 1.2, 1.4};
+	float output[3];
 	int length = 3;
-	float output[length];
+	kalman_state kstate;
+	kstate.q = 0.1;
+	kstate.r = 0.1;
+	kstate.p = 0.1;
+	kstate.k = 0;
+	kstate.x = 0;
 	
-	kstate->x = input[0];*/
+	input[0] = 1.0;
+	input[1] = 1.2;
+	input[2] = 1.4;
+	
+	kstate.x = input[0];
 	
 	// calling the assembly
 	
 	// calling the c
-	//Kalmanfilter_C(input, output, kstate, length);
-	example* ex;
-	ex->a = 10;
-	ex->b = 4;
-	ex->c = 2;
-	input[0] = 5;
-	input[1] = 10;
-	input[2] = 15;
-	exampleFilter(input, output, ex, 3);
-	
+	Kalmanfilter_C(input, output, &kstate, length);
+
 	return 0;
 }
 
