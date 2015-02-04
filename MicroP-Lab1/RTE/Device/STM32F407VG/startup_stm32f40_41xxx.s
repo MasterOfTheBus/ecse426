3f07a7a867a4c9d2e03e5dadf6fc0c86c9f7800c
Meng-Yin <meng.tao@mail.mcgill.ca>
2015-02-04 14:48:24 -0500

    Revert "trying to get c and asm to mix"
    This reverts commit 75dab4711a251195fd0c1063ef1b80aee2fc570c.

--- MicroP-Lab1/RTE/Device/STM32F407VG/startup_stm32f40_41xxx.s
+++ MicroP-Lab1/RTE/Device/STM32F407VG/startup_stm32f40_41xxx.s
@@ -177,7 +177,9 @@ __Vectors_Size  EQU  __Vectors_End - __Vectors
 Reset_Handler    PROC
                  EXPORT  Reset_Handler             [WEAK]
 ;        IMPORT  SystemInit
-        IMPORT  __main
+;        IMPORT  __main
+
+
 		IMPORT Kalmanfilter_asm
 		LDR.W 	R0,	=0xE000ED88
 		LDR		R1,	[R0]
@@ -187,9 +189,9 @@ Reset_Handler    PROC
 		ISB
 ;                 LDR     R0, =SystemInit
 ;                 BLX     R0
-                 LDR     R0, =__main
+;                 LDR     R0, =__main
 
-				;LDR		R1,	=Kalmanfilter_asm
+				LDR		R0,	=Kalmanfilter_asm
 
                  BX      R0
                  ENDP
