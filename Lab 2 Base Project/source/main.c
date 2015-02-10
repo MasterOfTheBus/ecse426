e51fc7cd2144cf6b1e5819552b4e2e8304c99d70
Sidney Ng <yendisng@gmail.com>
2015-02-10 14:41:16 -0500

    initilizing gpio for leds

--- Lab 2 Base Project/source/main.c
+++ Lab 2 Base Project/source/main.c
@@ -4,6 +4,19 @@
 
 int main(){
 	
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+	
+	GPIO_InitTypeDef gpio;
+	gpio.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+	gpio.GPIO_Mode = GPIO_Mode_OUT;
+	gpio.GPIO_Speed = GPIO_Speed_100MHz;
+	gpio.GPIO_OType = GPIO_OType_PP;
+	gpio.GPIO_PuPd =GPIO_PuPd_NOPULL;
+	
+	GPIO_Init(GPIOD, &gpio);
+	
+	GPIO_SetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
+	
 	while(1){
 	}
 	
